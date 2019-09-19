249ea13ae4bde633ce43ef7cd06c5ee9d5a73ea5
Marianne Cordova <macoam00@hotmail.com>
2019-09-19 07:46:15 -0700

    Agregar archivos de proyecto.

---
+++ Interaccion/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interaccion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lblHolaMundo.Text = "Adiós mundo";
+            lblHolaMundo.FontSize = 29;
+            lblHolaMundo.Foreground = Brushes.Red;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
